bc1bb86fe6b1b44c3353bcf71aa2bb7b2eb16814
Dvir Cohen <dvir906@gmail.com>
2023-03-16 01:35:24 +0200

    add Footer

---
+++ client/src/layout/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container mx-auto text-center my-5 text-xs'>@ 2023 - <span className='text-primary'>[Dvir Cohen] </span>- All right Reserved</div>
+  )
+}
+
+export default Footer
