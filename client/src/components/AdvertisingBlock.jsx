a45892f3e43fa8a27d7f0647511e95ed297ab346
Dvir Cohen <dvir906@gmail.com>
2023-03-16 01:29:54 +0200

    adding Advertising Block widget

---
+++ client/src/components/AdvertisingBlock.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdvertisingBlock = () => {
+  return (
+     <div className="p-5 my-3 h-48 flex bg-card-background justify-center items-center"> 300X250 </div>
+  )
+}
+
+export default AdvertisingBlock
--- client/src/pages/home/MainWidgets.jsx
+++ client/src/pages/home/MainWidgets.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { PrimaryButton, DangerButton } from "../../components/common/Buttons";
 import LatestNews from "../../components/LatestNews";
 import LiveStreams from "../../components/LiveStreams";
+import AdvertisingBlock from "../../components/AdvertisingBlock";
 const MainWidgets = () => {
   return (
     <div className="bg-page-background my-4 p-5 rounded-sm grid grid-cols-4 gap-3">
@@ -11,6 +12,7 @@ const MainWidgets = () => {
 
       <div>
         <LiveStreams />
+        <AdvertisingBlock />
       </div>
     </div>
   );
